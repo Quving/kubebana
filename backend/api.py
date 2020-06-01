c8c1b73ae35080a97a96fd7263b66fd8dec6c811
Vinh Ngu <vinh-ngu@hotmail.com>
2020-06-01 04:51:54 +0200

    Add api-endpoint '/deployments'.

--- backend/api.py
+++ backend/api.py
@@ -6,7 +6,7 @@ from kubeapi import KubeApi
 app = FlaskAPI(__name__)
 
 
-@app.route('/nodes', methods=['GET'])
+@app.route('/nodes/', methods=['GET'])
 def list_nodes():
     label_selector = request.args.get('selector', default=None, type=str)
 
@@ -14,6 +14,14 @@ def list_nodes():
     kubenodes = kubeapi.get_nodes(node_label_selector=label_selector)
 
     return [kubenode.to_dict() for kubenode in kubenodes]
+
+
+@app.route('/deployments/', methods=['GET'])
+def list_deployments():
+    namespace = request.args.get('namespace', default=None, type=str)
+
+    kubeapi = KubeApi()
+    return kubeapi.get_deployments(namespace=namespace)
 
 
 if __name__ == "__main__":
