b49bfb44093d6a7fec22e631d3ac86213b822c92
Vinh Ngu <vinh-ngu@hotmail.com>
2020-06-03 04:54:24 +0200

    Update card styling.

--- frontend/src/components/PodCard.vue
+++ frontend/src/components/PodCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-        <v-card class="mx-auto" min-width="300" max-width="300" outlined>
+        <v-card class="mx-auto" min-width="300" max-width="300" outlined raised>
             <v-list-item>
                 <v-list-item-content>
                     <div class="overline mb-4">POD</div>
