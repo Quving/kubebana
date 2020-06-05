a02bc409ee2bd42745f88408210a5e447bd2213f
Vinh Ngu <vinh-ngu@hotmail.com>
2020-06-05 03:07:06 +0200

    Configure server-api endpoint via envs.

--- web/src/config.js
+++ web/src/config.js
@@ -1,7 +1,9 @@
+const apiHostProtocol = process.env.KUBEBANA_API_HOST_PROTO ?? 'http'
+const apiHostPort = process.env.KUBEBANA_API_HOST_PORT ?? '5000';
+const apiHost = process.env.KUBEBANA_API_HOST ?? 'localhost';
 const envs = {
-    apiHostUrl: "http://localhost:5000/"
+    apiHostUrl: `${apiHostProtocol}://${apiHost}:${apiHostPort}/`
 
 }
-
 
 exports.envs = envs;
