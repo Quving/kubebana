dd2e0049750b82889dc3fa9223ced4ccbc5ee888
Vinh Ngu <vinh-ngu@hotmail.com>
2020-07-02 02:49:32 +0200

    Persist login information in cache.

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,16 +1,21 @@
 // src/store.js
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 export default new Vuex.Store({
     strict: true,
+    plugins: [createPersistedState()],
     state: {
         jwtToken: null,
     },
     getters: {
         isAuthenticated(state) {
             return state.jwtToken !== null;
+        },
+        jwtToken(state) {
+            return state.jwtToken;
         }
     },
     mutations: {
